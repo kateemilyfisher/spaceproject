5c7e757459f53f551bdcaa5be4eafa049da9ea31
Kate F <katef@bafta.org>
2020-02-07 14:35:36 +0000

    Adding the Picture of the day

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,7 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityRepository;
 use App\Repository\AstronautRepository;
 use App\Repository\SpaceStationRepository;
+use App\Repository\PictureRepository;
 use App\Entity\Position;
+use App\Entity\Picture;
 
 
 
@@ -22,19 +24,28 @@ class IndexController extends AbstractController
         //$em = $this->getEntityManager();
         $positionRepo = $this->getDoctrine()->getRepository('App:Position');
         $astronautRepo = $this->getDoctrine()->getRepository('App:Astronaut');
+        $pictureRepo = $this->getDoctrine()->getRepository('App:Picture');
         $astronauts = $astronautRepo->findAll();
 
         $results = $positionRepo->findAll();
+        $pictureOfTheDay = $pictureRepo->findAll();
 
         // this needs fixing
-        $position = new Position();
-        $latitude = $position->getLatitude();
-        $longitude = $position->getLongitude();
+        // $position = new Position();
+        // $latitude = $position->getLatitude();
+        // $longitude = $position->getLongitude();
+        //
+        // // why these no work
+        // $pictureOfTheDay = new Picture();
+        // $url = $pictureOfTheDay->getUrl();
+        // $explanation = $pictureOfTheDay->getExplanation();
+        // $date = $pictureOfTheDay->getDate();
 
 
         return $this->render('index/index.html.twig', [
             'astronauts' => $astronauts,
             'positions' => $results,
+            'pictureOfTheDay' => $pictureOfTheDay,
             'controller_name' => 'IndexController',
         ]);
     }
